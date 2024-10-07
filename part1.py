ec0448aa7239a37d23e1438d17eed2429f2bb101
Alexrazumovich <alexrazumovich@gmail.com>
2024-10-07 23:51:00 +0300

    dz_commit

--- part1.py
+++ part1.py
@@ -11,15 +11,36 @@ import pandas as pd
 # df=pd.DataFrame(data)
 # print(df)
 # =========
-# df=pd.read_csv('World-happiness-report-2024.csv')
-# # print(df.head())
-# # print(df.tail())
-# # print(df.info())
-# # print(df.describe())
-# # print(df['Country name'])
-# # print(df[['Country name','Regional indicator']])
+df=pd.read_csv('WorldsBestRestaurants.csv')
+print(df.head())
+# print(df.tail())
+print(df.info())
+print(df.describe())
+# print(df['Country name'])
+# print(df[['Country name','Regional indicator']])
 # print(df[df['Healthy life expectancy']>0.7])
-df = pd.read_csv('hh.csv')
-
-df['Test']=[new for new in range(29)]
+# ========================
+# df = pd.read_csv('hh.csv')
+#
+# df['Test']=[i for i in range(50)]
+# print(df)
+#
+# df.drop('Test',axis=1,inplace=True)
+# print(df)
+# =========================
+df=pd.read_csv('dz.csv')
 print(df)
+
+df.fillna(0,inplace=True)
+print(df)
+
+group=df.groupby('City')['Salary'].mean()
+print(group)
+# ======================
+# data={
+#     'Name':['Alice','Bob','Charlie'],
+#     'Age':[25,30,35],
+#     'City':['New York','LA','Chicago'],
+# }
+# df=pd.DataFrame(data)
+# df.to_csv('output.csv',index=False)
